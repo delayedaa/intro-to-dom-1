3321a6f396bb608ee17e641b05d9a3bb74b220dc
delayedaa <delayedaa@gmail.com>
2020-10-22 14:22:58 -0500

    [app.js] Add solutions to the "DOM Intro" codealong

--- app.js
+++ app.js
@@ -2,31 +2,96 @@
 // Select the specified parts of the DOM and console.log them
 // use document.getElementById, document.getElementsByClassName, document.getElementsByTagName
 
-// the mammals section
-// the birds section
-// all divs with the class animal
-// the first animal div
-// the last animal div
-// all imgs
-// the number of imgs on the page
-// all labels
-// all the labels, one at a time
+// // the mammals section
+// let mammalsSection = document.getElementById('mammals');
+// console.log(mammalsSection);
+// // the birds section
+// let birdsSection = document.getElementById('birds');
+// console.log(birdsSection);
+// // all divs with the class animal
+// let animals = document.getElementsByClassName('animal');
+// console.log(animals);
+// // the first animal div
+// let firstAnimal = document.getElementsByClassName('animal');
+// console.log(firstAnimal[0]);
+// // the last animal div
+// let lastAnimal = document.getElementsByClassName('animal');
+// console.log(lastAnimal.length - 1);
+// // all imgs
+// let imgs = document.getElementsByTagName('img');
+// console.log(imgs);
+// // the number of imgs on the page
+// console.log(imgs.length);
+// // all labels
+// let labels = document.getElementsByTagName('label');
+// console.log(labels);
+// // all the labels, one at a time
+// for (let i = 0; i < labels.length; i++) {
+//   console.log(labels[i]);
+// }
+
 
 // Do the zoo tour one more time, this time with document.querySelector and document.querySelectorAll
+// the mammals section
+let mammalsSection = document.querySelector('#mammals');
+console.log(mammalsSection);
+// the birds section
+let birdsSection = document.querySelector('#birds');
+console.log(birdsSection);
+// all divs with the class animal
+let animals = document.querySelectorAll('.animal');
+console.log(animals);
+// the first animal div
+let firstAnimal = document.querySelector('.animal:nth-of-type(1)');
+console.log(firstAnimal);
+// the last animal div
+let lastAnimal = document.getElementsByClassName('animal');
+console.log(lastAnimal.length - 1);
+// all imgs
+let imgs = document.getElementsByTagName('img');
+console.log(imgs);
+// the number of imgs on the page
+console.log(imgs.length);
+// all labels
+let labels = document.getElementsByTagName('label');
+console.log(labels);
+// all the labels, one at a time
+for (let i = 0; i < labels.length; i++) {
+  console.log(labels[i]);
+}
 
 
 // Changing parts of the zoo!
 
 // The owl is the coolest animal. Give the owl img a gold border to show this
+let owl = document.querySelector('#owl');
+owl.style.border = '5px solid gold';
+
 // Change the label for the bunny to "Stylish Bunny"
+let bunnyLabel = document.querySelector('label[for="bunny"]');
+// bunnyLabel.textContent = 'Stylish Bunny';
+
 // Once with innerText, then again with innerHTML. When using innerHTML, create a new div inside the label tag
+bunnyLabel.innerHTML = '<div>Stylish Bunny</div>';
+
 // Give all images a border-radius of 3em
+let allImages = document.querySelectorAll('img');
+for (let i = 0; i < allImages.length; i++) {
+  allImages[i].style.borderRadius = '3em';
+}
 
 // The parrot is being rotated for a newer, flashier model: https://images.pexels.com/photos/40984/animal-ara-macao-beak-bird-40984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500. Change the img's src to reflect this. Use a direct attribute assignment, not the setAttribute method
+let parrot = document.querySelector('#parrot');
+parrot.src = 'https://images.pexels.com/photos/40984/animal-ara-macao-beak-bird-40984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
 // Change our sheep image to: https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500. Use setAttribute
+document.querySelector('#sheep').setAttribute('src', 'https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
 
 // Give all labels a text-decoration of underline, a font-size of 2em, and a color of darkgreen. Create a new class to handle this, and apply it to all the labels.
+let labels2 = document.querySelectorAll('label');
+for (let i = 0; i < labels2.length; i++) {
+  labels2[i].classList.add('fancy-label');
+}
 
 // Remove the animal class from all divs that have it
 
